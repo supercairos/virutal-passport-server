e6f6b83ed27104e59ab40ff549717fd35ff95ada
Romain Caire <romain.caire@doro.com>
2015-12-27 15:26:38 +0100

    Update DB config

--- config/db.js
+++ config/db.js
@@ -4,14 +4,14 @@ var logger = require('../libs/logger.js');
 
 var mongoose_uri = process.env.OPENSHIFT_MONGODB_DB_URL || config.get("mongoose:uri")
 
-mongoose.connect(mongoose_uri);
+mongoose.connect(mongoose_uri + config.get("mongoose:name"));
 
 mongoose.connection.on('error', function callback() {
 	logger.error('Could not connect to %s', mongoose_uri + config.get("mongoose:name"));
 });
 
 mongoose.connection.once('open', function callback () {
-	logger.info('Database is connected to %s', mongoose_uri);
+	logger.info('Database is connected to %s', mongoose_uri + config.get("mongoose:name"));
 });
 
 
