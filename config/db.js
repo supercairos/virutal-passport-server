8601d37113643d5254065ea1f6bedb48c4f238a0
Romain Caire <romain.caire@doro.com>
2015-12-27 14:52:58 +0100

    Update Config (bis repetita)

--- config/db.js
+++ config/db.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 var config = require('./config.js');
 var logger = require('../libs/logger.js');
 
+var server_port = process.env.OPENSHIFT_MONGODB_DB_URL || config.get("mongoose:uri")
+
 mongoose.connect(config.get("mongoose:uri"));
 
 mongoose.connection.on('error', function callback() {
