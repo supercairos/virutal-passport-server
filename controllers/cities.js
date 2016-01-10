763e47abd5ff9292c30bd49c2a19b55caac7b52f
Romain Caire <romain.caire@doro.com>
2016-01-10 23:39:38 +0100

    sERVER

--- controllers/cities.js
+++ controllers/cities.js
@@ -25,11 +25,72 @@ module.exports = function(app) {
 							country: element.countryName,
 							latitude: element.lat,
 							longitude: element.lng,
+							picture: 'http://' + req.headers.host + '/cities/' + element.lat + '/' + element.lng;
 						});
 					});
 				}
 				
 				res.status(200).json( out ).end();
+			});
+		}
+	);
+	
+	app.get('/cities/:lat/:lon', 
+		// passport.authenticate('bearer', { session: false }), 
+		function(req, res){
+			Flickr.tokenOnly({
+			  api_key: "50ce3b858e5d2e847d26ce7ee56f7bce",
+			  secret: "86aa46059054f8ef"
+			}, function(error, flickr) {
+				if (error) {
+					return next(new NetworkException(err.message, 1));
+				}
+	
+				flickr.photos.search({
+					sort : "interestingness-desc",
+					privacy_filter : 1,
+					accuracy : 9,
+					safe_search : 1,
+					content_type : 1,
+					group_id : "1463451@N25",
+					lat : req.params.lat,
+					lon : req.params.lon,
+					extras : "original_format",
+					per_page : 1,
+					format : "json",
+					nojsoncallback : 1
+				}, function(err, result) {
+					if (err) {
+						return next(new NetworkException(err.message, 1));
+					}
+					// do something with result
+					var url;
+					if(result.photos.photo.length > 0) {
+						var photo = result.photos.photo[0];
+						flickr.photos.getSizes({
+							photo_id : photo.id
+						}, function(err, result) {
+							if (err) {
+								return next(new NetworkException(err.message, 1));
+							}
+							
+							var sizes = result.sizes.size;
+							var current = 0;
+							var url;
+							sizes.forEach(function (element, index, array) {
+								if((element.width * element.height) > current) {
+									current = element.width * element.height;
+									url = element.source;
+								}
+							});
+							
+							log.info("Url : ", url);
+							req.pipe(request(url)).pipe(res)
+						});
+					} else {
+						res.status(404).end();
+					}
+				});
 			});
 		}
 	);
